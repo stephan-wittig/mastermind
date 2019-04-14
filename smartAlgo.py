b59718d6ed23280624e50d6467bbf77a7668f7bd
Stephan <ste.wittig@gmail.com>
2019-04-14 21:11:56 +0200

    Started work on genetic algorithm

--- smartAlgo.py
+++ smartAlgo.py
@@ -4,12 +4,19 @@ import string
 class SmartAlgo():
     def __init__(self, numOfDigits):
         self.numOfDigits = numOfDigits
+        self.drawNumber = 0
+        self.distribution = {} # for each digit : 0
 
     def nextDraw(self):
-        nextDraw = []
-        for i in range(5):
-            nextDraw.append("".join([random.choice(string.digits) for j in range(self.numOfDigits)]))
-        return nextDraw
+        #ToDo: Zu  switch ändern
+        if self.drawNumber > 1:
+            #Send geneticly generated string
+        elif self.drawNumber = 1:
+            #Send string of 5s, 6s ... 9s only
+        elif self.drawNumber = 0:
+            #Send string of 0s, 1s ... 4s only
 
     def results(self, results):
-        pass
+        
+        
+        self.drawNumber ++
